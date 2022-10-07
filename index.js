5d7affad247f718e7fe6310408ec3a8ceb0660f1
JOE <johnorjuela@yahoo.com>
2022-10-07 14:49:03 -0500

    Iniciando con javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+const displayValorAnterior = document.getElementById('numanterior');
+const displayValorActual = document.getElementById('numactual');
+const botonNumeros = document.querySelectorAll('.numero');
+const botonOperador = document.querySelectorAll('.operador');
+
+const Calculadora = new Calculadora();
+
+botonNumeros.forEach
